06db74a60fafeaf866cad3dac2e087f7f6e13f1e
X1A0 <1287545325@qq.com>
2024-12-26 21:44:28 +0800

    nihao

---
+++ main/test/hello world.cpp
@@ -0,0 +1,25 @@
+//#pragma GCC optimize(3)
+#include <bits/stdc++.h>
+//#define int LL
+#define endl '\n'
+#define size_t int
+#define all(v) v.begin(), v.end()
+using namespace std;
+typedef long long LL;
+typedef vector<int> vint;
+typedef vector<vint> vvint;
+typedef vector<string> vstr;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+
+signed main()
+{
+    //ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    int t = 1;
+    cin >> t;
+    for (size_t i = 0; i < 40; i++) {
+        cout << i << " " << endl;
+    }
+    
+    return 0;
+}
